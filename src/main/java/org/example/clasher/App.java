599397ece7b5411995e0231b7d447eaadfd12502
Quanuanc <quanuanc@outlook.com>
2022-09-25 10:14:39 +0800

    refactor: Stop using spring boot

--- src/main/java/org/example/clasher/App.java
+++ src/main/java/org/example/clasher/App.java
@@ -1,13 +1,8 @@
 package org.example.clasher;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
-@EnableScheduling
 public class App {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        System.out.println("Hello");
     }
 }
